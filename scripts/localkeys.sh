687703fdcb2bb66ae613f216de982da151171e04
Grady Broyles <slobeck@gmail.com>
2026-08-08 16:48:39 -0700

    Routine push: Sat Aug  8 04:48:39 PM PDT 2026

--- scripts/localkeys.sh
+++ scripts/localkeys.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
+LOGS="$HOME/.tmux/logs"
 main() {
-  for orig in "$( tmux list-keys | grep 'next-window' | grep -iv 'wheel' )"; do
-    local keystr="$( awk '{printf '%s', $4}' <<< $orig )"
-    local cmdstr="$( awk '{printf '%s', $5}' <<< $orig )"
-    printf "%s %s\n" "$keystr" "$cmdstr" >> ~/temp-tmux.log
+  local keystr cmdstr
+  for orig in $( tmux list-keys | grep 'next-window' | grep -iv 'wheel' ); do
+    keystr="$( awk '{printf '%s', $4}' <<< "$orig" )"
+    cmdstr="$( awk '{printf '%s', $5}' <<< "$orig" )"
+    [[ ! -d "$LOGS" ]] && mkdir -pv "$LOGS"
+    printf "%s %s\n" "$keystr" "$cmdstr" >> "$LOGS"
   done
 }
 main
